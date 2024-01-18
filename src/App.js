36ea35fd544b3378ed59330117a0e95c5122363a
Carly Geehr <carly@getlocalmotion.com>
2024-01-18 17:19:43 -0600

    now with the actual auth part saved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  import React from 'react';
+  import logo from './logo.svg';
+  import './App.css';
+  import { Amplify } from 'aws-amplify';
+  import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+  import awsconfig from './aws-exports';
+  Amplify.configure(awsconfig);
+  function App({ signOut }) {
+    return (
+        <div>
+        <h1>Thankyou for doing verification</h1>
+        <h2>My Content</h2>
+         <button onClick={signOut}>Sign out</button>
+      </div>
+    );
+  }
+  export default withAuthenticator(App);
