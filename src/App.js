a51c2ecbb4e932409fdf8862dbc74a55ce36cfad
Carly Geehr <carly@getlocalmotion.com>
2024-01-19 18:07:41 -0600

    working auth again, starting to work on integrating the user information

--- src/App.js
+++ src/App.js
@@ -1,18 +1,93 @@
 
-  import React from 'react';
-  import logo from './logo.svg';
-  import './App.css';
-  import { Amplify } from 'aws-amplify';
-  import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
-  import awsconfig from './aws-exports';
+import react,{ useState ,useEffect}  from 'react'
+
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator} from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+
+import { generateClient } from "aws-amplify/api";
+import { listUsers } from "./graphql/queries";
+import { createUser } from './graphql/mutations';
+
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import { getCurrentUser } from 'aws-amplify/auth';
+import { getUrl } from 'aws-amplify/storage';
+
   Amplify.configure(awsconfig);
+  const client = generateClient()
+
+
+
+
+
   function App({ signOut }) {
+
+    const [userList,setUserList]=useState([])
+
+    const getImageUrl = async(filename)=>{
+      const getUrlResult = await getUrl({
+             key: filename,
+             options: {
+                 accessLevel:'public'
+             },
+         })
+         
+ 
+         return getUrlResult.url.href
+         
+    }
+
+    async function validateUser()  {
+      console.log('validate user')
+  
+      try{
+            // List all items
+        const userRows = await client.graphql({
+            query: listUsers,
+            variables:{}
+        });
+
+        setUserList(userRows.data.listUsers.items)
+
+        const currentUser = await getCurrentUser()
+        console.log(currentUser)
+
+        const userAttributes = await fetchUserAttributes()
+        console.log(userAttributes)
+  
+    } catch (err) {
+        console.log('Error getting user')
+        console.log(err)
+    }
+  }
+
+  
+    
+
+useEffect(()=>{
+  validateUser()
+  console.log('useEffect')
+
+
+},[])
+    
+
+ 
+   
+
     return (
-        <div>
+
+    <div className="App">
+
+      
+      <header className="App-header">
         <h1>Thankyou for doing verification</h1>
         <h2>My Content</h2>
          <button onClick={signOut}>Sign out</button>
-      </div>
+         {userList.map((each)=><p>{each}</p>)}
+      </header>
+    </div>
     );
   }
   export default withAuthenticator(App);
